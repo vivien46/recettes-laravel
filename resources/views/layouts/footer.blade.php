6da249cf5319cbf12644d3955fed520b3f5f7f36
vivien <vlad-vivien@orange.fr>
2024-09-21 19:33:40 +0200

    fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,11 @@
-<footer class="bg-gray-800 text-white p-4 mt-auto">
-    <div class="container mx-auto text-center">
-        <p>&copy; 2024 Mon Application. Tous droits réservés.</p>
+<footer class="bg-gray-800 text-white p-4 mt-auto w-full">
+    <div class="container mx-auto flex flex-col md:flex-col justify-between items-center md:text-center">
+        <p class="text-semibold">&copy; 2024 Mon Application. Tous droits réservés.</p>
+        <div class="flex flex-wrap justify-center md:justify-end space-x-4">
+            <a href="#" class="hover:text-blue-400  text-semibold">Mentions Légales</a>
+            <a href="#" class="hover:text-blue-400  text-semibold">Politique de Confidentialité</a>
+            <a href="#" class="hover:text-blue-400  text-semibold">Contact</a>
+        </div>
     </div>
 </footer>
+
