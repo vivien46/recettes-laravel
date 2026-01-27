d7fb90f791377e8bebf5a50cb6cd0e3a05da3162
vivien <vlad-vivien@orange.fr>
2026-01-27 18:49:16 +0100

    adjust copyright place and copy year'

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="bg-gray-800 text-white p-4 mt-auto w-full">
     <div class="container mx-auto flex flex-col md:flex-col justify-between items-center md:text-center">
-        <p class="text-semibold">&copy; 2024 Mon Application. Tous droits réservés.</p>
         <div class="flex flex-wrap justify-center md:justify-end space-x-4">
             <a href="#" class="hover:text-blue-400  text-semibold">Mentions Légales</a>
             <a href="#" class="hover:text-blue-400  text-semibold">Politique de Confidentialité</a>
             <a href="#" class="hover:text-blue-400  text-semibold">Contact</a>
         </div>
+        <p class="text-semibold">&copy; 2024 - {{ date('Y') }} - Mon Application. Tous droits réservés.</p>
     </div>
 </footer>
 
